3469ab8d4e861d4d205d2bae487181c12b1cdbb9
saturn1.mc <saturn1.mc@86d79716-7a43-0410-b766-bfdaccba7ff9>
2008-01-21 16:04:21 +0000

    toString + operator <<

--- Scene.h
+++ Scene.h
@@ -10,6 +10,8 @@
 #ifndef SCENE_H
 #define SCENE_H
 
+#include <iostream>
+#include <sstream>
 #include <list>
 #include <cmath>
 
@@ -22,9 +24,9 @@ class Scene{
 private:
 	
 	Observer *observer;
-	std::list<Object *> objects;
 	Image *img;
 	Color *background;
+	std::list<Object *> objects;
 	
 public:
 	
@@ -34,12 +36,49 @@ public:
 		background = _background;
 	}
 	
-	int getH() {return img->getH();}
-	int getW() {return img->getW();}
+	int getH(void) const {return img->getH();}
+	int getW(void) const {return img->getW();}
+	Observer &getObserver(void) const {return *observer;}
+	Image &getImage(void) const {return *img;}
+	Color &getBackground(void) const {return *background;}
+	std::list<Object *> getObjects(void) const {return objects;}
 	
 	void add(Object *obj);
 	void rayTrace(void);
 	double focal(void);
 };
+
+template <class charT, class traits> std::basic_ostream<charT,traits> &operator << (std::basic_ostream<charT,traits>& strm, const Scene &scene){
+	/* From : "C++ Standard Library, The A Tutorial And Reference - Nicolai M. Josuttis - Addison Wesley - 1999" */
+	
+	/* string stream
+	* - with same format
+	* - without special field width
+	*/
+	std::basic_ostringstream<charT,traits> s;
+	s.copyfmt(strm);
+	s.width(0);
+	
+	// fill string stream
+	s << "##########################################" << std::endl;
+	s << "Scene :" << std::endl;
+	s << "##########################################" << std::endl;
+	s << scene.getObserver() << std::endl;
+	s << scene.getImage() << std::endl;
+	s << "Background color :" << scene.getBackground() << std::endl;
+	s << "********************************" << std::endl;
+	s << "Objects :" << std::endl;
+	s << "********************************" << std::endl;
+	for(list<Object *>::iterator iter = scene.getObjects().begin(); iter != scene.getObjects().end(); ++iter){
+		s << (*iter)->toString() << endl;
+	}
+	s << "********************************" << std::endl;
+	s << "##########################################" << std::endl;
+	
+	// print string stream
+	strm << s.str();
+	
+	return strm;
+}
 
 #endif //SCENE_H
