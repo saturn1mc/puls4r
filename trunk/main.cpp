788e7101000995ebe490389473311c53eb2063cb
saturn1.mc <saturn1.mc@86d79716-7a43-0410-b766-bfdaccba7ff9>
2008-01-21 19:29:43 +0000

    Correction ray()

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -17,9 +17,9 @@ using namespace std;
 
 int main (int argc, char * const argv[]) {
 	Observer *obs = new Observer(new Point(6.0, 2.0, 6.0), new Point(0.0, 0.0, 0.0), M_PI/4.0);
-	Scene *scene = new Scene(obs, new Image("test", 50, 50), new Color(1.0,1.0,1.0));
+	Scene *scene = new Scene(obs, new Image("/Users/camille/Desktop/test.bmp", 300, 300), new Color(0.0, 0.0, 0.0));
 	
-	scene->add(new Sphere(new Point(10.0, 0.0, 20.0), 10.0));
+	scene->add(new Sphere(new Color(1.0, 0.0, 0.0), new Point(0.0, 0.0, 0.0), 1.0));
 	
 	cout << *scene << endl;
 	
