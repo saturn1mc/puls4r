119d56e45bf23dd52bff07c50b9dd08c773f75cd
saturn1.mc <saturn1.mc@86d79716-7a43-0410-b766-bfdaccba7ff9>
2009-01-16 13:50:35 +0000

    Preparing Java version : Creating basic project structure

---
+++ Java/Puls4r.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author cmaurice2
+ *
+ */
+public class Puls4r {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
